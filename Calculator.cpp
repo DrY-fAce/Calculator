f911517e8951fc2e671a865010ca3b5f98604c47
DrY-fAce <160291708+DrY-fAce@users.noreply.github.com>
2025-02-14 12:23:52 +0300

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2;
+    char operation;
+
+    cout << "Введите первое число: ";
+    cin >> num1;
+    cout << "Введите операцию (+, -, *, /): ";
+    cin >> operation;
+    cout << "Введите второе число: ";
+    cin >> num2;
+
+    switch (operation) {
+        case '+':
+            cout << "Результат: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Результат: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Результат: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << "Результат: " << num1 / num2 << endl;
+            } else {
+                cout << "Ошибка: деление на ноль!" << endl;
+            }
+            break;
+        default:
+            cout << "Неверная операция!" << endl;
+    }
+
+    return 0;
+}
